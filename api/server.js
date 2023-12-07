a99ea27dbba0c86f94ea0e5f051b0e8fb666b082
Lndubose <lndubose13@gmail.com>
2023-12-07 14:41:34 -0800

    Learn about custom middlewares

--- api/server.js
+++ api/server.js
@@ -1,21 +1,39 @@
-const express = require('express'); // importing a CommonJS module
-
-const hubsRouter = require('./hubs/hubs-router.js');
+const express = require("express"); // importing a CommonJS module
+const morgan = require("morgan");
+const hubsRouter = require("./hubs/hubs-router.js");
 
 const server = express();
 
+function customMorgan(req, res, next) {
+  console.log(`you made a ${req.method} request`);
+  next();
+}
+
+function shortCircuit(req, res, next) {
+  res.json("the request was short circuited!");
+}
+
+function addFriend(req, res, next) {
+  req.friend = "Lady Gaga";
+  next();
+}
+
 server.use(express.json());
+server.use(morgan("dev"));
+server.use(customMorgan);
+// server.use(shortCircuit);
+server.use(addFriend);
 
-server.use('/api/hubs', hubsRouter);
+server.use("/api/hubs", hubsRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send(`
-    <h2>Hubs API</h2>
+    <h2>Hubs API ${req.friend}</h2>
     <p>Welcome to the Hubs API</p>
   `);
 });
 
-server.use('*', (req, res) => {
+server.use("*", (req, res) => {
   // catch all 404 errors middleware
   res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
 });
